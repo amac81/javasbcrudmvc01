f8e32cff761280c7ffbb8ae2f0f5718cb3fcf7ce
amac81 <arnaldocanelas@gmail.com>
2023-09-05 11:42:03 +0100

    JDBC authentication; bcrypt password encryption

--- src/main/java/pt/bitclinic/javasbcrudmvc01/config/SecurityConfig.java
+++ src/main/java/pt/bitclinic/javasbcrudmvc01/config/SecurityConfig.java
@@ -1,91 +1,62 @@
 package pt.bitclinic.javasbcrudmvc01.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+	// Add support for JDBC
+
 	@Bean
-	InMemoryUserDetailsManager userDetailsManager() {
-		UserDetails jonh = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
-
-		UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER")
-				.build();
-
-		UserDetails susan = User.builder().username("susan").password("{noop}test123")
-				.roles("EMPLOYEE", "MANAGER", "ADMIN").build();
-		
-		UserDetails amac = User.builder().username("amac").password("{noop}test123")
-				.roles("EMPLOYEE", "ADMIN").build();
-
-		return new InMemoryUserDetailsManager(jonh, mary, susan, amac);
+	UserDetailsManager userDetailsManager(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
 	}
-	
-	@Bean
-    WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
-	
+
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(configurer -> configurer
 				.requestMatchers("/").hasRole("EMPLOYEE")
 				.requestMatchers("/employees/leaders/**").hasRole("MANAGER")
 				.requestMatchers("/employees/systems/**").hasRole("ADMIN")
+				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+				.anyRequest().authenticated())
 
-                .anyRequest().authenticated())
-				
-				.formLogin(form -> form
-						.loginPage("/showMyLoginPage")
-						.loginProcessingUrl("/authenticateTheUser")
+				.formLogin(form -> form.loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser")
 						.permitAll())
 				.logout(logout -> logout.permitAll())
 				.exceptionHandling(configurer -> configurer.accessDeniedPage("/denied"));
 
-
 		return http.build();
 	}
-
-	// Add support for JDBC
-
+	
 	/*
-	 * @Bean UserDetailsManager userDetailsManager(DataSource dataSource) { return
-	 * new JdbcUserDetailsManager(dataSource); }
+	 * In memory users
 	 * 
-	 * @Bean SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	 * http.authorizeHttpRequests(configurer -> configurer
-	 * .requestMatchers(HttpMethod.GET, "/home").permitAll()
+	 * @Bean InMemoryUserDetailsManager userDetailsManager() { UserDetails jonh =
+	 * User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").
+	 * build();
 	 * 
-	 * //.requestMatchers(HttpMethod.GET, "/api/clients").hasRole("EMPLOYEE")
-	 * //.requestMatchers(HttpMethod.GET, "/api/clients/**").hasRole("EMPLOYEE")
-	 * //.requestMatchers(HttpMethod.POST, "/api/clients").hasRole("MANAGER")
-	 * //.requestMatchers(HttpMethod.PUT, "/api/clients/**").hasRole("MANAGER")
-	 * //.requestMatchers(HttpMethod.DELETE, "/api/clients/**").hasRole("ADMIN"));
-	 * );
+	 * UserDetails mary =
+	 * User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE",
+	 * "MANAGER") .build();
 	 * 
-	 * // Use HTTP basic authentication http.httpBasic(Customizer.withDefaults());
+	 * UserDetails susan =
+	 * User.builder().username("susan").password("{noop}test123") .roles("EMPLOYEE",
+	 * "MANAGER", "ADMIN").build();
 	 * 
-	 * // Disable CSRF protection http.csrf(csrf -> csrf.disable());
+	 * UserDetails amac = User.builder().username("amac").password("{noop}test123")
+	 * .roles("EMPLOYEE", "ADMIN").build();
 	 * 
-	 * /* Cross-Site Request Forgery (CSRF) is an attack that forces authenticated
-	 * users to submit a request to a Web application against which they are
-	 * currently authenticated. CSRF attacks exploit the trust a Web application has
-	 * in an authenticated user.
-	 *
-	 * In general CSRF protection is not required for stateless REST API's that use
-	 * POST, PUT, DELETE, and/or PATCH.
-	 *
-	 *
-	 * return http.build(); }
+	 * return new InMemoryUserDetailsManager(jonh, mary, susan, amac); }
 	 */
 
 }
