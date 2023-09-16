e042a1a1f81e57892772fddcd3e97bc2efb7c474
amac81 <arnaldocanelas@gmail.com>
2023-09-16 11:57:20 +0100

    department-form.html and list-department.html update

--- src/main/java/pt/bitclinic/javasbcrudmvc01/entities/Department.java
+++ src/main/java/pt/bitclinic/javasbcrudmvc01/entities/Department.java
@@ -15,7 +15,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
